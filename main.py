e736f178ed29faf9f3857fbe9799a2a164ded70c
Marius <marius1890@yahoo.com>
2025-10-25 17:03:50 +0300

    #

---
+++ main.py
@@ -0,0 +1,9 @@
+import img2pdf
+
+
+fisiere_jpg: list[str] = ["adev_master.jpg", "diploma_licenta.jpg"]
+
+for fisier in fisiere_jpg:
+    new_fisier: str = fisier[0: fisier.find(".")]
+    with open(f"{new_fisier}.pdf", "wb") as f:
+        f.write(img2pdf.convert(f"{new_fisier}.jpg"))
